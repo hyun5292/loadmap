01bc520c2829dd915db2e9f5693167c869add590
박수현 <57443458+hyun5292@users.noreply.github.com>
2023-02-21 23:46:36 +0900

    로드맵 1강 완료
    1-1. HTML, CSS 이해하기
    1-2. 기본 태그 구조 이해하기
    1-3. head 태그 알아보기
    1-4. heading 태그 알아보기
    1-5. 주석과 a, fieldset, input, button
    1-6. 리스트와 이미지 태그 알아보기
    
    -> 내가 듣기엔 너무 간단하지만 그래두...

---
+++ memo.js
@@ -0,0 +1,19 @@
+/*
+HTML
+	- Hyper Text Markup Language
+	- 웹의 구조 + 컨텐츠
+*/
+/*
+CSS
+	- Cascading Style Sheets
+	- 디자인
+*/
+/*
+JavaScript
+	- 웹의 모든 것
+*/
+
+/*
+잡담
+	- 오 콘솔에 document.head.parentNode.removeChild(document.head); 치니까 css 사라지네
+*/
